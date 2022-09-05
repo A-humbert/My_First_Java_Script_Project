e5fa4a7cb4a5169c9ed098b2bc259228b9257d04
Trafalgar05 <amahhumbert@gmail.com>
2022-09-05 13:45:55 +0000

    les differents evenements sur les formulaires

--- java_scrit_with_web/partie2/index.js
+++ java_scrit_with_web/partie2/index.js
@@ -1,4 +1,4 @@
-/*  let button = document.querySelector(".Boutton")
+/*   let button = document.querySelector(".Boutton")
 
 button.addEventListener("click", (e) => {
     e.target.style.color="red"
@@ -6,20 +6,82 @@ button.addEventListener("click", (e) => {
     e.target.style.fontSize="20px"
 })
  
- button.addEventListener("mouseover",(e) => (e.target.textContent = "fuck you🤷‍♀️🤦‍♂️")) 
+ //button.addEventListener("mouseover",(e) => (e.target.textContent = "fuck you🤷‍♀️🤦‍♂️")) 
 
 
 
 let firstbutton = document.querySelector(".first")
 
 firstbutton.addEventListener("click", (a) =>{
-    a.target.style.color ="#000"
-    a.target.style.fontSize ="15px"
+    a.target.style.color ="wheat"
+    a.target.style.fontSize ="20px"
     a.target.style.fonWeight ="bold"
 }) 
-
-firstbutton.addEventListener("mouseover", (a) => (
-    a.target.textContent = "fuck you baby🤦‍♂️")
-)
-
  */
+/* firstbutton.addEventListener("mouseover", (a) => (
+    a.target.textContent = "fuck you baby🤦‍♂️")
+) */
+
+
+
+let suscribes = {
+    username: "",
+    password: "",
+    sexe: "",
+    contactWays: [], 
+    country: ""
+}
+
+// reaction des elements du formulaire
+
+let inputUsername = document.getElementById("username")
+let inputPassword = document.getElementById("pwd")
+let inputSexe = document.getElementsByName("sexe")
+
+let inputMail = document.getElementById("mail")
+let inputTel = document.getElementById("tel")
+let inputBp = document.getElementById("Bp")
+
+let selectCountry = document.getElementById("country")
+
+
+// input events
+
+inputUsername.addEventListener("input",(e) =>{
+    
+    suscribes.username = e.target.value
+} )
+
+inputPassword.addEventListener("input", (e) =>{
+suscribes.password = e.target.value
+})
+
+for(let myInput of inputSexe){
+    myInput.addEventListener("change",(e) => {
+        suscribes.sexe = e.target.value
+    })
+}
+
+inputBp.addEventListener("change", (e) =>{
+    e.target.checked? suscribes.contactWays.push("Bp") : null;
+
+})
+
+inputMail.addEventListener("change", (e) =>{
+    e.target.checked? suscribes.contactWays.push("mail") : null;
+})
+
+inputTel.addEventListener("change", (e) =>{
+    e.target.checked? suscribes.contactWays.push("tel") : null;
+})
+
+selectCountry.addEventListener("change",(e)=>{
+suscribes.country = e.target.value
+})
+
+document.querySelector("form").addEventListener("submit",(e)=>{
+    e.preventDefault()
+    console.log(suscribes)
+})
+
+    
